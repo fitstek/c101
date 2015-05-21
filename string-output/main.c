57f1b2f74bb253555aee5ccd6b909d88d8038a9e
Fitsum Teklehaimanot <fitsum.teklehaimanot@gmail.com>
2015-05-21 15:07:45 +0100

    Initial Commit

---
+++ string-output/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  string-output
+//
+//  Created by Fitsum Teklehaimanot on 21/05/2015.
+//  Copyright (c) 2015 Fitsum Teklehaimanot. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
