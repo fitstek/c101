5495ac4dc5ca5c5d447329baef22931b6712ca5d
Fitsum Teklehaimanot <fitsum.teklehaimanot@gmail.com>
2015-06-01 17:56:19 +0100

    C unions

--- string-output/main.c
+++ string-output/main.c
@@ -298,3 +298,36 @@
 //  printf( "Book subject : %s\n", book->subject);
 //  printf( "Book book_id : %d\n", book->book_id);
 //}
+
+////Unions and accessing their values
+//
+//#include <string.h>
+//
+//union Data
+//{
+//  int i;
+//  float f;
+//  char  str[20];
+//};
+//
+//int main( )
+//{
+//  union Data data;
+//  
+//  data.i = 20;
+//  printf("Stored integer is %d\n", data.i);
+//  
+//  data.f = 20;
+//  printf("Stored float is %f\n", data.f);
+//  
+//  strcpy(data.str, "Hello");
+//  printf("Stored character is %s\n", data.str);
+//  
+////  printf( "Memory size occupied by data : %lu\n", sizeof(data));
+////  printf("Stored integer is %d\n", data.i); // We won't get the right values because the final value
+////  printf("Stored float is %f\n", data.f); // assigned has occupied the memory location
+////  printf("Stored character is %s\n", data.str);
+//  
+//  return 0;
+//}
+
