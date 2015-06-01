7bf40b698d3566eb2e1133c8dc34b1601c995c4b
Fitsum Teklehaimanot <fitsum.teklehaimanot@gmail.com>
2015-06-01 14:56:25 +0100

    structure tutorial 1

--- string-output/main.c
+++ string-output/main.c
@@ -148,3 +148,61 @@
 //  printf( "The address of the variable x by using x's address %x\n", &x );
 //  getchar();
 //}
+
+//structures in C
+
+//struct example {
+//  int x;
+//};
+//
+//int main() {
+//  struct example an_example;
+//  
+//  an_example.x = 20;
+//  printf("x is %d", an_example.x);
+//  return 0;
+//}
+
+// Another example of using structs
+
+#include <string.h>
+
+struct Books {
+  char title[50];
+  char author[50];
+  char subject[50];
+  int book_id;
+  
+};
+
+int main( )
+{
+  struct Books Book1;        /* Declare Book1 of type Book */
+  struct Books Book2;        /* Declare Book2 of type Book */
+  
+  /* book 1 specification */
+  strcpy(Book1.title, "C programming");
+  strcpy(Book1.author, "Nuha Ali");
+  strcpy(Book1.subject, "C programming Tutorial");
+  Book1.book_id = 6495407;
+  
+  /* book 2 specification */
+  strcpy(Book2.title, "Telecom Billing");
+  strcpy(Book2.author, "Zara Ali");
+  strcpy(Book2.subject, "Telecom Billing Tutorial");
+  Book2.book_id = 6495700;
+  
+  /* print Book1 info */
+  printf( "Book 1 title : %s\n", Book1.title);
+  printf( "Book 1 author : %s\n", Book1.author);
+  printf( "Book 1 subject : %s\n", Book1.subject);
+  printf( "Book 1 book_id : %d\n", Book1.book_id);
+  
+  /* print Book2 info */
+  printf( "Book 2 title : %s\n", Book2.title);
+  printf( "Book 2 author : %s\n", Book2.author);
+  printf( "Book 2 subject : %s\n", Book2.subject);
+  printf( "Book 2 book_id : %d\n", Book2.book_id);
+  
+  return 0;
+}
