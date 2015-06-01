bc282f38bd08c404cfcc1baab8d7b5aa94a498a5
Fitsum Teklehaimanot <fitsum.teklehaimanot@gmail.com>
2015-06-01 15:02:00 +0100

    structures as function input

--- string-output/main.c
+++ string-output/main.c
@@ -165,6 +165,49 @@
 
 // Another example of using structs
 
+//#include <string.h>
+//
+//struct Books {
+//  char title[50];
+//  char author[50];
+//  char subject[50];
+//  int book_id;
+//  
+//};
+//
+//int main( )
+//{
+//  struct Books Book1;        /* Declare Book1 of type Book */
+//  struct Books Book2;        /* Declare Book2 of type Book */
+//  
+//  /* book 1 specification */
+//  strcpy(Book1.title, "C programming");
+//  strcpy(Book1.author, "Nuha Ali");
+//  strcpy(Book1.subject, "C programming Tutorial");
+//  Book1.book_id = 6495407;
+//  
+//  /* book 2 specification */
+//  strcpy(Book2.title, "Telecom Billing");
+//  strcpy(Book2.author, "Zara Ali");
+//  strcpy(Book2.subject, "Telecom Billing Tutorial");
+//  Book2.book_id = 6495700;
+//  
+//  /* print Book1 info */
+//  printf( "Book 1 title : %s\n", Book1.title);
+//  printf( "Book 1 author : %s\n", Book1.author);
+//  printf( "Book 1 subject : %s\n", Book1.subject);
+//  printf( "Book 1 book_id : %d\n", Book1.book_id);
+//  
+//  /* print Book2 info */
+//  printf( "Book 2 title : %s\n", Book2.title);
+//  printf( "Book 2 author : %s\n", Book2.author);
+//  printf( "Book 2 subject : %s\n", Book2.subject);
+//  printf( "Book 2 book_id : %d\n", Book2.book_id);
+//  
+//  return 0;
+//}
+
+// structure as function arguments(refactor the code above)
 #include <string.h>
 
 struct Books {
@@ -177,6 +220,8 @@ struct Books {
 
 int main( )
 {
+  void printBook(struct Books book);
+  
   struct Books Book1;        /* Declare Book1 of type Book */
   struct Books Book2;        /* Declare Book2 of type Book */
   
@@ -193,16 +238,17 @@ int main( )
   Book2.book_id = 6495700;
   
   /* print Book1 info */
-  printf( "Book 1 title : %s\n", Book1.title);
-  printf( "Book 1 author : %s\n", Book1.author);
-  printf( "Book 1 subject : %s\n", Book1.subject);
-  printf( "Book 1 book_id : %d\n", Book1.book_id);
+  printBook(Book1);
   
   /* print Book2 info */
-  printf( "Book 2 title : %s\n", Book2.title);
-  printf( "Book 2 author : %s\n", Book2.author);
-  printf( "Book 2 subject : %s\n", Book2.subject);
-  printf( "Book 2 book_id : %d\n", Book2.book_id);
+  printBook(Book2);
   
   return 0;
+}
+
+void printBook(struct Books book){
+  printf( "Book title : %s\n", book.title);
+  printf( "Book author : %s\n", book.author);
+  printf( "Book subject : %s\n", book.subject);
+  printf( "Book book_id : %d\n", book.book_id);
 }
